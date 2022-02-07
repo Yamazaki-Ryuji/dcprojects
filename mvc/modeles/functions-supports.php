79f8bcb94bd31f56316b54661e0646cf9c160d3e
Charles Letang <genjuro23000@gmail.com>
2022-02-07 14:01:29 +0100

    php

--- mvc/modeles/functions-supports.php
+++ mvc/modeles/functions-supports.php
@@ -9,6 +9,7 @@ add_action('after_setup_theme','SC_theme_prefix_setup');
 add_theme_support('post-thumbnails');
 
 /* MENU */
+register_nav_menus(['main-menu'=>'Menu Principal']);
 register_nav_menus(['header-top'=>'Menu Header']);
 register_nav_menus(['footer-left'=>'Menu Footer Left']);
 register_nav_menus(['footer-middle'=>'Menu Footer Middle']);
