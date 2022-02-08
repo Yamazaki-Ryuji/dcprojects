e4328a4132a65b178ef3638831c1929e7f9c05ab
Mamadou <mamad33sylla@gmail.com>
2022-02-08 08:51:48 +0100

    page etudiant a propos

---
+++ etudiant-apropos.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Template Name: Page etudiant apropos
+ */
+?>
+
+<?php
+get_header();
+include __VUES_DIR__.'/vues-etudiant-apropos.php';
+get_footer();
+?>
---
+++ mvc/vues/vues-etudiant-apropos.php
@@ -0,0 +1,188 @@
+<div class="titre">
+        <h1>
+           <div class="titre-1">Dc projects,</div> 
+           <div class="titre2"> tu connais ?</div>
+        </h1>
+    </div>
+    <div class="apropos-photo-text">
+        <div class="apropos-photo">
+            <img class="image1" src="img/a-propos1.jpg" alt="">
+            <img class="image2" src="img/a-propos2.jpg" alt="">
+            <img class="decoration-x" src="img/Décoration.svg" alt="">
+        </div>
+        <p class="text">
+            Depuis sa création en 2016 l’association DCPROJECTS accompagne les Grands Projets à Digital Campus Bordeaux dans le bon déroulement principalement logistique de ces derniers chaque année.
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+        </p>
+    </div>
+    <div class="membre">
+        <div class="gauche">
+            <div class="grade-nom">
+                <p>DIRECTEUR</p>
+                <h2>Sylvain coudert</h2>
+            </div>
+            <img src="img/Sylvain-illustration.svg" alt="">
+        </div>
+        <div class="droite">
+            <div class="grade-nom2">
+                <p>DIRECTEUR</p>
+                <h2>Sylvain coudert</h2>
+            </div>
+            <p class="membre-text">Aussi surnommé Dieu Sylvain. La légende raconte qu’il possède une pièce secrète remplit de legos. Offrez-en lui et vous obtiendrez peut-être des points de compétence en développement, mais bien sur on ne vous promet rien ! </p>
+            <div class="competences">
+                <div class="competence">
+                    <img src="img/Compétence-Crack-en-développement.jpg" alt="">
+                    <b><p>CRACK EN <br> DEVELOPPEMENT</p></b>
+                </div> 
+                <div class="competence">
+                    <img src="img/Compétence-Constructeur-de-legos.jpg" alt="">
+                 <b><p>CONSTRUCTEUR  <br> 
+                        DE LEGOS </p></b>   
+                        
+                </div> 
+                <div class="competence">
+                    <img src="img/Compétence-Dieu-des-bachelors.jpg" alt="">
+                  <b><p>DIEU DES  <br> BACHELORS</p></b>  
+                </div> 
+                </div>
+                <div class="grade">
+                    <div class="niveau1"></div>
+                    <div class="niveau2"></div>
+                    <div class="niveau3"></div>
+                    <div class="niveau4"></div>
+                    <div class="niveau5"></div>
+                </div>
+                <p>MYTHIQUE</p>
+        </div>
+    </div>
+    <div class="membre">
+        <div class="gauche">
+            <div class="grade-nom">
+                <p>TRÉSORIER</p>
+            <h2>Lucas Ceuillet</h2>
+            </div>
+            <img src="img/Illustration-Lucas.jpg" alt="">
+        </div>
+        <div class="droite">
+            <div class="grade-nom2">
+                <p>TRÉSORIER</p>
+            <h2>Lucas Ceuillet</h2>
+            </div>
+            <p class="membre-text">En plus d’être le trésorier de l’association, Lucas est le grand manitou des projets à digital campus ! Faites attention à vous, il vous convertira à l’utilisation de Notion en un clin d’œil et détail non négligeable : il détient également les clés de la gaming room ! </p>
+            <div class="competences">
+                <div class="competence">
+                    <img src="img/Compétence-Gourou-de-notion.jpg" alt="">
+                  <b><p>Gourou de<br>
+                    notion</p></b>  
+                </div> 
+                <div class="competence">
+                    <img src="img/Compétence-maître-des-plannings.jpg" alt="">
+                  <b><p> Maître des  <br> Plannings</p></b>  
+                </div> 
+                <div class="competence">
+                    <img src="img/Compétence-Coordinateur-top-level.jpg" alt="">
+                   <b><p> Coordinateur <br>
+                     Top level</p></b>
+                </div>
+            </div>
+                <div class="grade">
+                    <div class="niveau1"></div>
+                    <div class="niveau2"></div>
+                    <div class="niveau3"></div>
+                    <div class="niveau4"></div>
+                    <div class="niveau5"></div>
+                </div>
+                <p>MASTERCLASS</p>
+        </div>
+    </div>
+    <div class="membre">
+        <div class="gauche">
+            <div class="grade-nom">
+                <p>SECRÉTAIRE</p>
+                <h2>MARINE Aitali</h2>
+            </div>
+            <img src="img/Illustration-Marine.jpg" alt="">
+        </div>
+        <div class="droite">
+            <div class="grade-nom2">
+                <p>SECRÉTAIRE</p>
+                <h2>MARINE Aitali</h2>
+            </div>
+            <p class="membre-text">Connu dans le milieu comme la rein de la com, elle gère l’ensemble des communications de tous le réseau DC, le tout en français, mais aussi en Japonais ! 
+            Ne vous fiez pas à son regard de glace, elle est toujours bienveillante et prête à donner un coup de main.
+            Son point faible ? Trop forte !</p>
+            <div class="competences">
+                <div class="competence">
+                    <img src="img/Compétence-comm.jpg" alt="">
+                  <b><p>Machine de <br></b>  
+                    La comm’</p>
+                </div> 
+                <div class="competence">
+                    <img src="img/Compétence-multitache.jpg" alt="">
+                   <b><p>Multitâche</p></b> 
+                </div> 
+                <div class="competence">
+                    <img src="img/Compétence-japonais.jpg" alt="">
+                   <b><p>Parle <br>
+                    Japonais !</p></b>
+                </div> 
+            </div>
+                <div class="grade">
+                    <div class="niveau1"></div>
+                    <div class="niveau2"></div>
+                    <div class="niveau3"></div>
+                    <div class="niveau4"></div>
+                    <div class="niveau5"></div>
+                </div>
+                <p>MYTHIQUE</p>
+            
+        </div> 
+    </div>
+    <div class="apropos-lgp">
+        <div class="description-lgp">
+            <h3>LES GRANDS PROJETS</h3>
+            <p>En mode agence !
+                Depuis la création de l’école en 2011, chaque mardi d’octobre à mars, les étudiants du Bachelor, de l’année 
+                Prépa et du Mastère de l’ensemble du réseau Digital Campus se transforment en véritables professionnels 
+                experts du web.
+                Comme dans une agence digitale, en équipes de 4 à 10 personnes (de tous niveaux confondus), chacun tient 
+                son rôle et travaille en collaboration sur un projet grandeur nature, porté par un partenaire de l’école : une 
+                grande entreprise, une start-up, une association ou une institution de renommée.
+                A travers ces projets réels, accompagnés de coordinateurs professionnels, les étudiants apprennent à gérer 
+                un projet dans son ensemble, développer des réflexes professionnels, s’organiser, travailler en équipe et en 
+                autonomie et à savoir présenter devant un client.
+                Cette année plus de 80 projets ont été menés sur toute la France. Venez assister aux présentations finales !</p>
+        </div>    
+    </div>
+    <div class="hrz-slide">
+        <div class="image-hrz">
+            <img src="img/GPDC-2020-2021.jpg" alt="">
+            <div class="content">
+                <h4>#GPDC 2020-2021</h4>
+                <p class=""></p>
+            </div>
+        </div>
+            <div class="image-hrz">
+                <img src="img/GPDC-2020-2021.jpg" alt="">
+                <div class="content">
+                    <h4>#GPDC 2020-2021</h4>
+                    <p class=""></p>
+                </div>
+            </div>
+            <div class="image-hrz">
+                <img src="img/GPDC-2020-2021.jpg" alt="">
+                <div class="content">
+                    <h4>#GPDC 2020-2021</h4>
+                    <p class=""></p>
+                </div>
+            </div>
+        <div class="image-hrz">
+            <img src="img/GPDC-2020-2021.jpg" alt="">
+            <div class="content">
+                <h4>#GPDC 2020-2021</h4>
+                <p class=""></p>
+            </div>
+        </div>
+          
+    </div>
